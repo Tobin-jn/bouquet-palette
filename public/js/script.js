20e414dad396abb48aa4e30a3b4d9f54cd2106bd
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-27 15:14:01 -0700

    Lock colors when generating new palette

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,13 @@ const $createPalette = $('.create-palette')
 
 
 $createPalette.on('click', generatePalette)
+$('.color1').on('click', lockColor)
+$('.color2').on('click', lockColor)
+$('.color3').on('click', lockColor)
+$('.color4').on('click', lockColor)
+$('.color5').on('click', lockColor)
+
+
 
 function generatePalette() {
   const codeOne = generateHexCode()
@@ -17,13 +24,28 @@ function generatePalette() {
 
 function updateColors(codeOne, codeTwo, codeThree, codeFour, codeFive) {
 
-  $('.color1').css('background', codeOne)
-  $('li:nth-child(2)').css('background', codeTwo)
-  $('.color3').css('background', codeThree)
-  $('.color4').css('background', codeFour)
-  $('.color5').css('background', codeFive)
+  if (!$('.color1').attr('disabled')) {
+    $('.color1').css('background', codeOne)
+  }
+  if (!$('.color2').attr('disabled')) {
+    $('.color2').css('background', codeTwo)
+  }
+  if (!$('.color3').attr('disabled')) {
+    $('.color3').css('background', codeThree)
+  }
+  if (!$('.color4').attr('disabled')) {
+    $('.color4').css('background', codeFour)
+  }
+  if (!$('.color5').attr('disabled')) {
+    $('.color5').css('background', codeFive)
+  }
 }
 
+function lockColor() {
+  console.log('test')
+  $(this).attr("disabled", true)
+  console.log($(this))
+}
  
 
 
