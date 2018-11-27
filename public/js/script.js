37179a2a76f231935839ca60f173b2e4002a703d
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-27 14:12:46 -0700

    Generate 6 unique hex codes

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,17 @@
-const hexCode = require('../utilities/utilities.js')
+// const hexCode = require('../utilities/utilities.js');
 
 const $createPalette = $('.create-palette')
 
 $createPalette.on('click', generatePalette)
 
 function generatePalette() {
+  const codeOne = generateHexCode()
+  const codeTwo = generateHexCode()
+  const codeThree = generateHexCode()
+  const codeFour = generateHexCode()
+  const codeFive = generateHexCode()
+
+  console.log(codeOne, codeTwo, codeThree, codeFour, codeFive)
   //create hex code
 }
 
@@ -12,4 +19,11 @@ function generatePalette() {
 //numbers 0-255
 // ABCDEF
 
-
+  const generateHexCode = () => {
+  const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
+  let hexCode = `#`
+  for(let i = 0; i < 6; i++){
+    hexCode += hexValues[Math.round(Math.random() * 15)]
+  }
+  return hexCode
+}
--- public/utilities/utilities.js
+++ public/utilities/utilities.js
@@ -1,5 +1,4 @@
-
-function generateHexCode() {
+module.exports.generateHexCode = () => {
   const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
   let hexCode = `#`
   for(let i = 0; i < 6; i++){
