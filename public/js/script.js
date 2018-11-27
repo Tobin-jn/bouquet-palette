00de14da8909d8682f4937ecc6063450b1869e0b
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-27 13:49:21 -0700

    Write function to generate random hex codes

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,15 @@
+const hexCode = require('../utilities/utilities.js')
+
 const $createPalette = $('.create-palette')
 
-$createPalette.on('click', () => console.log('test'))
+$createPalette.on('click', generatePalette)
+
+function generatePalette() {
+  //create hex code
+}
 
 //create a hex code at random
 //numbers 0-255
 // ABCDEF
+
+
