25ff90679d21d9fad716c1ea9d87ef0c1bde7499
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-27 18:23:44 -0700

    Add hex codes to user interface

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,6 @@ $('.color4').on('click', lockColor)
 $('.color5').on('click', lockColor)
 
 
-
 function generatePalette() {
   const codeOne = generateHexCode()
   const codeTwo = generateHexCode()
@@ -26,18 +25,23 @@ function updateColors(codeOne, codeTwo, codeThree, codeFour, codeFive) {
 
   if (!$('.color1').attr('disabled')) {
     $('.color1').css('background', codeOne)
+    $('.code1').text(codeOne).css('color', codeOne)
   }
   if (!$('.color2').attr('disabled')) {
     $('.color2').css('background', codeTwo)
+    $('.code2').text(codeTwo).css('color', codeTwo)
   }
   if (!$('.color3').attr('disabled')) {
     $('.color3').css('background', codeThree)
+    $('.code3').text(codeThree).css('color', codeThree)
   }
   if (!$('.color4').attr('disabled')) {
     $('.color4').css('background', codeFour)
+    $('.code4').text(codeFour).css('color', codeFour)
   }
   if (!$('.color5').attr('disabled')) {
     $('.color5').css('background', codeFive)
+    $('.code5').text(codeFive).css('color', codeFive)
   }
 }
 
@@ -53,7 +57,7 @@ function lockColor() {
 
 
 const generateHexCode = () => {
-  const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
+  const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
   let hexCode = `#`
   for(let i = 0; i < 6; i++){
     hexCode += hexValues[Math.round(Math.random() * 15)]
