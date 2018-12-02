df0d7640a9e8a9cf99015e2d5249bce983aea783
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-02 08:39:42 -0700

    Lint errors in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,39 @@
-import {paletteHexCodes} from './utilities.js'
-import {postProject, getProjects, postPalette, getPalettes,  deletePalette} from './apiCalls.js'
+import {paletteHexCodes} from './utilities.js';
+import {postProject, getProjects, postPalette, getPalettes,  deletePalette} from './apiCalls.js';
 
 $(getProjects)
 $(document).ready(function() {
   if (JSON.parse(localStorage.getItem('currentColors'))) {
-    let colors= JSON.parse(localStorage.getItem('currentColors'));
-    currentColors = {...colors}
+    let colors= JSON.parse(localStorage.getItem('currentColors'));;
+    currentColors = {...colors};
     updateColors(colors.hex1, colors.hex2, colors.hex3, colors.hex4, colors.hex5)
-    localStorage.clear()
+    localStorage.clear();
   } else {
     generatePalette()
   }
 });
 
-let currentColors = {}
+let currentColors = {};
 
 
-$('.create-palette').on('click', generatePalette)
+$('.create-palette').on('click', generatePalette);
 
-$('.flower1').on('click', lockColor)
-$('.flower2').on('click', lockColor)
-$('.flower3').on('click', lockColor)
-$('.flower4').on('click', lockColor)
-$('.flower5').on('click', lockColor)
+$('.flower1').on('click', lockColor);
+$('.flower2').on('click', lockColor);
+$('.flower3').on('click', lockColor);
+$('.flower4').on('click', lockColor);
+$('.flower5').on('click', lockColor);
 
-$('.save-project-btn').on('click', saveProject)
-$('.save-palette-btn').on('click', savePalette)
+$('.save-project-btn').on('click', saveProject);
+$('.save-palette-btn').on('click', savePalette);
 
-$('.new-palette-input').on('keyup', enableBtn)
-$('.new-project-input').on('keyup', enableBtn)
+$('.new-palette-input').on('keyup', enableBtn);
+$('.new-project-input').on('keyup', enableBtn);
 
-$('.show-palettes-btn').on('click', showPalettes)
+$('.show-palettes-btn').on('click', showPalettes);
 
-$('.project-palettes').on('click', removePalette)
-$('.project-palettes').on('click', selectPalette)
+$('.project-palettes').on('click', removePalette);
+$('.project-palettes').on('click', selectPalette);
 
 
  
@@ -49,7 +49,7 @@ function enableBtn() {
 
 
 function generatePalette() {
-  const codes = paletteHexCodes()
+  const codes = paletteHexCodes();
 
   updateColors(codes.hex1, codes.hex2, codes.hex3, codes.hex4, codes.hex5)
 }
@@ -57,22 +57,22 @@ function generatePalette() {
 
 function lockColor() {
   if (!$(this).attr('disabled')) {
-    $(this).attr("disabled", true)
-    $(this).css("border-bottom", "solid 3px #ba5a19")
+    $(this).attr("disabled", true);
+    $(this).css("border-bottom", "solid 3px #ba5a19");
   } else {
-    $(this).removeAttr("disabled")
-    $(this).css("border-bottom", "solid 3px white")
+    $(this).removeAttr("disabled");
+    $(this).css("border-bottom", "solid 3px white");
   }
 }
 
 
 function removePalette() {
   let paletteId; 
-  const projectId = parseInt($('#project-palette-select').val(), 10)
+  const projectId = parseInt($('#project-palette-select').val(), 10);
 
   if(event.target.className === 'delete-palette'){
-    paletteId = event.target.attributes.value.value
-    event.target.parentElement.remove()
+    paletteId = event.target.attributes.value.value;
+    event.target.parentElement.remove();
     deletePalette(paletteId, projectId)
   }
 }
@@ -91,15 +91,15 @@ function saveColors() {
 
 function savePalette(e) {
   e.preventDefault()
-  const name = $('.new-palette-input').val()
-  const id = parseInt($('#project-select').val(), 10)
+  const name = $('.new-palette-input').val();
+  const id = parseInt($('#project-select').val(), 10);
   const newPalette = {
     name, 
     ...currentColors, 
     project_id: id
-  }
+  };
   postPalette(newPalette)
-  $('.new-palette-input').val('')
+  $('.new-palette-input').val('');
   $('.save-palette-btn').prop('disabled', true);
 }
 
@@ -108,18 +108,18 @@ function saveProject(e) {
   e.preventDefault()
   localStorage.setItem('currentColors', JSON.stringify(currentColors));
 
-  let name = $('.new-project-input').val()
-  let projectName = { name }
+  let name = $('.new-project-input').val();
+  let projectName = { name };
   postProject(projectName)
 
-  $('.new-project-input').val('')
+  $('.new-project-input').val('');
   $('.save-project-btn').prop('disabled', true);
 }
 
 
 function selectPalette() {
   if(event.target.className === 'palette-name') {
-    const paletteColors = event.target.nextElementSibling.children
+    const paletteColors = event.target.nextElementSibling.children;
     updateColors(    
       paletteColors[0].dataset.id,
       paletteColors[1].dataset.id,
@@ -133,41 +133,41 @@ function selectPalette() {
 
 function showPalettes(e) {
   e.preventDefault()
-  const id = parseInt($('#project-palette-select').val(), 10)
+  const id = parseInt($('#project-palette-select').val(), 10);
   getPalettes(id)
 }
 
 
 function updateColors(codeOne, codeTwo, codeThree, codeFour, codeFive) {
   if (!$('.flower1').attr('disabled')) {
-    $('#flower1-petals1')[0].attributes.fill.value = codeOne
-    $('#flower1-petals2')[0].attributes.fill.value = codeOne
-    $('.code1').text(codeOne).css('color', codeOne)
-    $('.circle1').css('background', codeOne)
+    $('#flower1-petals1')[0].attributes.fill.value = codeOne;
+    $('#flower1-petals2')[0].attributes.fill.value = codeOne;
+    $('.code1').text(codeOne).css('color', codeOne);
+    $('.circle1').css('background', codeOne);
   }
   if (!$('.flower2').attr('disabled')) {
-    $('#flower2-petals1')[0].attributes.fill.value = codeTwo
-    $('#flower2-petals2')[0].attributes.fill.value = codeTwo
-    $('.code2').text(codeTwo).css('color', codeTwo)
-    $('.circle2').css('background', codeTwo)
+    $('#flower2-petals1')[0].attributes.fill.value = codeTwo;
+    $('#flower2-petals2')[0].attributes.fill.value = codeTwo;
+    $('.code2').text(codeTwo).css('color', codeTwo);
+    $('.circle2').css('background', codeTwo);
   }
   if (!$('.flower3').attr('disabled')) {
-    $('#flower3-petals1')[0].attributes.fill.value = codeThree
-    $('#flower3-petals2')[0].attributes.fill.value = codeThree
-    $('.code3').text(codeThree).css('color', codeThree)
-    $('.circle3').css('background', codeThree)
+    $('#flower3-petals1')[0].attributes.fill.value = codeThree;
+    $('#flower3-petals2')[0].attributes.fill.value = codeThree;
+    $('.code3').text(codeThree).css('color', codeThree);
+    $('.circle3').css('background', codeThree);
   }
   if (!$('.flower4').attr('disabled')) {
-    $('#flower4-petals1')[0].attributes.fill.value = codeFour
-    $('#flower4-petals2')[0].attributes.fill.value = codeFour
-    $('.code4').text(codeFour).css('color', codeFour)
-    $('.circle4').css('background', codeFour)
+    $('#flower4-petals1')[0].attributes.fill.value = codeFour;
+    $('#flower4-petals2')[0].attributes.fill.value = codeFour;
+    $('.code4').text(codeFour).css('color', codeFour);
+    $('.circle4').css('background', codeFour);
   }
   if (!$('.flower5').attr('disabled')) {
-    $('#flower5-petals1')[0].attributes.fill.value = codeFive
-    $('#flower5-petals2')[0].attributes.fill.value = codeFive
-    $('.code5').text(codeFive).css('color', codeFive)
-    $('.circle5').css('background', codeFive)
+    $('#flower5-petals1')[0].attributes.fill.value = codeFive;
+    $('#flower5-petals2')[0].attributes.fill.value = codeFive;
+    $('.code5').text(codeFive).css('color', codeFive);
+    $('.circle5').css('background', codeFive);
   }
   saveColors()
 }
