65baa21986a5b8822e02127396a0564405d916f2
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-15 21:00:50 -0700

    API test setup, get projects test

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 app.use(express.static('public'));
-
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palettes';
 
@@ -94,7 +93,7 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
-
+module.exports = app;
 
 
 
--- test/server.test.js
+++ test/server.test.js
@@ -1,37 +1,75 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expect = chai.expect
-const should = chai.should();
 const app = require('../server.js');
+const expect = chai.expect;
+const should = chai.should();
 const config = require('../knexfile')['test'];
 const database = require('knex')(config);
 
-chai.use(chaiHttp)
-
-//SETUP TEST ENVIRONMENT and TEST DATABASE
+chai.use(chaiHttp);
 
 
 describe('Client Routes', () => {
-  // it('should return the homepage', done => {
-  //   chai
-  //     .request(app)
-  //     .get('/')
-  //     .end((error, response) => {
-  //       response.should.have.status(200);
-  //       response.should.be.html;
-  //       done();
-  //     });
-  // });
-    it('should be true', (done) => {
-      //done is necessary for async testing to shut down the spun up server
-      expect(2).to.equal(2)
-      done()
+  it('should return a 404 for a route that does not exist', done => {
+    chai
+      .request(app)
+      .get('/badurl')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+    });
+  });
+  it('should return the homepage', done => {
+    chai
+      .request(app)
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+        done();
+      });
+  });
+});
+
+describe('API Routes', () => {
+  beforeEach(() =>
+    database.migrate
+      .rollback()
+      .then(() => database.migrate.latest())
+      .then(() => database.seed.run())
+  );
+
+  describe('GET /api/v1/projects', () => {
+    it('should return all the projects', done => {
+      chai
+        .request(app)
+        .get('/api/v1/projects')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body[0].should.have.property('name');
+          response.body[0].name.should.equal('Winter');
+          response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(1);
+          done()
+      })
     })
-    //dummy test can be done to see if everything is hooked up and then deleted
+  });
+
+
+
+  describe('/api/v1/projects/:id', () => {
+
+  });
+
+  describe('/api/v1/projects/:project_id/palettes', () => {
+
+  });
+
+  describe('/api/v1/projects/:project_id/palettes/:palette_id', () => {
+
+  });
 });
 
 
-
-// describe('API Routes', () => {
-
-// });
